151bdf0fc30207d8c861fd2305d860280908a0e7
Takashi Kominami <dev.kominami@gmail.com>
2021-03-14 14:36:59 +0900

    feat(serverless.ts): functionとresouceを外に定義

--- serverless.ts
+++ serverless.ts
@@ -1,6 +1,65 @@
 import type { AWS } from '@serverless/typescript';
 // import { handlerPath } from '@libs/handlerResolver';
 
+const hello =  {
+  handler: `src/functions/hello/handler.main`,
+  role: "s3EventRole",
+  events: [
+    {
+      s3: {
+        bucket: "${self:custom.s3Event.bucket}",
+        event: 's3:ObjectCreated:*',
+        rules: [
+          {
+            prefix: "${self:custom.s3Event.prefix}",
+            suffix: ''
+          }
+        ],
+        existing: true
+      }
+    }
+  ]
+}
+
+const s3EventRole = {
+  Type: 'AWS::IAM::Role',
+  Properties: {
+    Path: '/',
+    RoleName: "${self:provider.stage}S3EventRole",
+    AssumeRolePolicyDocument: {
+      Version: '2012-10-17',
+      Statement: [
+        {
+          Effect: 'Allow',
+          Principal: {
+            Service: [
+              'lambda.amazonaws.com'
+            ]
+          },
+          Action: 'sts:AssumeRole'
+        }
+      ]
+    },
+    Policies: [
+      {
+        PolicyName:  "${self:provider.stage}S3EventPolicy",
+        PolicyDocument: {
+          Version: '2012-10-17',
+          Statement: [
+            {
+              Effect: 'Allow',
+              Action: [
+                'S3:*'
+              ],
+              Resource: "arn:aws:s3:::${self:custom.s3Event.bucket}/*"
+            }
+          ]
+        }
+      }
+    ]
+  }
+}
+
 const serverlessConfiguration: AWS = {
   service: 's3-event',
   frameworkVersion: '2',
@@ -27,68 +86,9 @@ const serverlessConfiguration: AWS = {
     lambdaHashingVersion: '20201221',
   },
   // import the function via paths
-  functions: {
-    hello: {
-      handler: `src/functions/hello/handler.main`,
-      role: "s3EventRole",
-      events: [
-        {
-          s3: {
-            bucket: "${self:custom.s3Event.bucket}",
-            event: 's3:ObjectCreated:*',
-            rules: [
-              {
-                prefix: "${self:custom.s3Event.prefix}",
-                suffix: ''
-              }
-            ],
-            existing: true
-          }
-        }
-      ]
-    }
-  },
+  functions: { hello },
   resources: {
-    Resources: {
-      s3EventRole: {
-        Type: 'AWS::IAM::Role',
-        Properties: {
-          Path: '/',
-          RoleName: "${self:provider.stage}S3EventRole",
-          AssumeRolePolicyDocument: {
-            Version: '2012-10-17',
-            Statement: [
-              {
-                Effect: 'Allow',
-                Principal: {
-                  Service: [
-                    'lambda.amazonaws.com'
-                  ]
-                },
-                Action: 'sts:AssumeRole'
-              }
-            ]
-          },
-          Policies: [
-            {
-              PolicyName:  "${self:provider.stage}S3EventPolicy",
-              PolicyDocument: {
-                Version: '2012-10-17',
-                Statement: [
-                  {
-                    Effect: 'Allow',
-                    Action: [
-                      'S3:*'
-                    ],
-                    Resource: "arn:aws:s3:::${self:custom.s3Event.bucket}/*"
-                  }
-                ]
-              }
-            }
-          ]
-        }
-      }
-    }
+    Resources: { s3EventRole }
   }
 };
 
